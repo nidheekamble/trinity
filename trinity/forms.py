963f69de0ad0eb2836842dc08818bc3a068a61a2
nidheekamble <kamble.nidhee@gmail.com>
2019-06-05 15:16:57 +0530

    checkpoints added after reg; login not working

--- trinity/forms.py
+++ trinity/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField
+from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, IntegerField, RadioField, BooleanField, PasswordField, SubmitField, TextAreaField, SelectField, HiddenField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, Required, NumberRange, ValidationError
@@ -18,8 +18,13 @@ class UserForm(FlaskForm):
 class OrgForm(FlaskForm):
 	name = StringField('Name', validators = [DataRequired(), Length(max=30)])
 	email = StringField('Email', validators = [DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
 	types = [('0','Parties'), ('1', 'Wedding'), ('2', 'Corporate')]
 	kind = SelectField('Type', choices = types, validators = [Required()])
+	about = TextAreaField('About your organization', validators = [DataRequired()])
+	photo1 = FileField('Add picture of your services here',validators=[FileAllowed(['jpg', 'png'])])
+	photo2 = FileField('It gives the user a better preview',validators=[FileAllowed(['jpg', 'png'])])
+	photo3 = FileField('You may add pictures later too',validators=[FileAllowed(['jpg', 'png'])])
 	submit = SubmitField('Make Me Discoverable')
 
 	def validateEmail(self, Email):
@@ -27,3 +32,24 @@ class OrgForm(FlaskForm):
 		if user:
 			raise ValidationError('Email taken, enter a different one')
 
+class LoginForm(FlaskForm):
+	email = StringField('Email', validators = [DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	submit = SubmitField('Log In')
+
+class UpdateDetails(FlaskForm):
+	name = StringField('Name', validators = [DataRequired(), Length(max=30)])
+	email = StringField('Email', validators = [DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	types = [('0','Parties'), ('1', 'Wedding'), ('2', 'Corporate')]
+	kind = SelectField('Type', choices = types, validators = [Required()])
+	about = TextAreaField('About your organization', validators = [DataRequired()])
+	photo1 = FileField('Photo 1',validators=[FileAllowed(['jpg', 'png'])])
+	photo2 = FileField('Photo 2',validators=[FileAllowed(['jpg', 'png'])])
+	photo3 = FileField('Photo 3',validators=[FileAllowed(['jpg', 'png'])])
+	submit = SubmitField('Update details')
+
+	def validateEmail(self, Email):
+		user = User.query.filter_by(email=email.data).first()
+		if user:
+			raise ValidationError('Email taken, enter a different one')
--- trinity/routes.py
+++ trinity/routes.py
@@ -1,10 +1,15 @@
 import os
 import secrets
+import requests
+import hashlib
 from trinity import app, db
 from trinity.models import User, Organizer
-from trinity.forms import UserForm, OrgForm
+from trinity.forms import UserForm, OrgForm, UpdateDetails, LoginForm
 from sqlalchemy import or_, and_
-from flask import Flask, session, render_template, url_for, flash, redirect, request
+from sqlalchemy.orm import Session
+from flask import Flask, session, render_template, url_for, flash, redirect, request, send_from_directory
+from flask_login import login_user, current_user, logout_user, login_required
+from PIL import Image
 
 @app.route("/")
 @app.route("/home", methods = ['GET', 'POST'])
@@ -15,12 +20,47 @@ def home():
 def regOrg():
 	form = OrgForm()
 	if form.validate_on_submit():
-		organizer = Organizer(email=form.email.data, name=form.name.data, kind=form.kind.data)
+		
+		pw = (form.password.data)
+		s = 0
+		for char in pw:
+			a = ord(char) #ASCII
+			s = s+a #sum of ASCIIs acts as the salt
+		hashed_password = (str)((hashlib.sha512((str(s).encode('utf-8'))+((form.password.data).encode('utf-8')))).hexdigest())
+
+		organizer = Organizer(email=form.email.data, name=form.name.data, kind=form.kind.data, password = hashed_password)
 		organizer.type = 'organizer'
+
+		if form.photo1.data:
+			photo_file = save_photo(form.photo1.data)
+			organizer.photo1 = photo_file
+			photo1 = url_for('static', filename='organizer/' + organizer.photo1)
+		if form.photo2.data:
+			photo_file = save_photo(form.photo2.data)
+			organizer.photo1 = photo_file
+			photo2 = url_for('static', filename='organizer/' + organizer.photo2)
+		if form.photo3.data:
+			photo_file = save_photo(form.photo3.data)
+			organizer.photo1 = photo_file
+			photo3 = url_for('static', filename='organizer/' + organizer.photo3)
+
 		db.session.add(organizer)
 		db.session.commit()
+		print(organizer)
+
 		return redirect(url_for('login'))
 	return render_template('organizer.html', form=form)
+
+def save_photo(form_photo):
+	random_hex = secrets.token_hex(8)
+	_, f_ext = os.path.splitext(form_photo.filename)
+	photo_fn = random_hex + f_ext
+	photo_path = os.path.join(app.root_path, 'static\organizer', photo_fn)
+	output_size = (125, 125)
+	i = Image.open(form_photo)
+	i.thumbnail(output_size)
+	i.save(photo_path)
+	return photo_fn
 
 
 @app.route("/user", methods = ['GET', 'POST'])
@@ -31,11 +71,32 @@ def regUser():
 		user.type = 'user'
 		db.session.add(user)
 		db.session.commit()
+		print(user)
 		return redirect(url_for('find'))
 	return render_template('user.html', form=form)
 
 @app.route("/login", methods = ['GET', 'POST'])
 def login():
+	form = LoginForm(request.form)
+
+	if form.validate_on_submit():
+		organizer = Organizer.query.filter_by(email=form.email.data).first()
+		pw = (form.password.data)
+		s = 0
+		for char in pw:
+			a = ord(char) #ASCII
+			s = s+a #sum of ASCIIs acts as the salt
+		now_hash = (str)((hashlib.sha512((str(s).encode('utf-8'))+((form.password.data).encode('utf-8')))).hexdigest())
+
+		if (organizer and (organizer.password==now_hash)):
+			login_user(organizer)
+			print("hash correct")
+			redirect(url_for('account'))
+
+		else:
+			print('Nahin hua')
+			flash('Login Unsuccessful. Please check email and password', 'danger')
+
 	return render_template('login.html', form=form)
 
 
@@ -43,3 +104,29 @@ def login():
 def find():
 	return render_template('find.html)')
 
+@app.route("/account")
+def account():
+	form = UpdateDetails()
+	organizer = Organizer.query.filter_by(id=current_user.id).first()
+	if form.validate_on_submit():
+
+		organizer = Organizer(email=form.email.data, name=form.name.data, kind=form.kind.data)
+		organizer.type = 'organizer'
+
+		if form.photo1.data:
+			photo_file = save_photo(form.photo1.data)
+			organizer.photo1 = photo_file
+			photo1 = url_for('static', filename='organizer/' + organizer.photo1)
+		if form.photo2.data:
+			photo_file = save_photo(form.photo2.data)
+			organizer.photo1 = photo_file
+			photo2 = url_for('static', filename='organizer/' + organizer.photo2)
+		if form.photo3.data:
+			photo_file = save_photo(form.photo3.data)
+			organizer.photo1 = photo_file
+			photo3 = url_for('static', filename='organizer/' + organizer.photo3)
+
+		db.session.add(organizer)
+		db.session.commit()
+		print(organizer)
+	return render_template("account.html")
